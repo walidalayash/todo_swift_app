94a8927d4fb58b8c78f6c2280507b5600ae0c81b
Walid Alayash <walidalayash99@gmail.com>
2023-08-26 16:18:46 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by walid alayash on 26/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
