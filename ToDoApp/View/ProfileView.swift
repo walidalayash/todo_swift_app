69d7a49304a1f6cced92a5c5a566a197c15fae62
Walid Alayash <walidalayash99@gmail.com>
2023-08-29 03:17:45 +0200

    Profile page & log out

--- ToDoApp/View/ProfileView.swift
+++ ToDoApp/View/ProfileView.swift
@@ -13,18 +13,58 @@ struct ProfileView: View {
         NavigationView{
             VStack{
                 
-            }.navigationTitle("Profile")
-                .toolbar{
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "plus")
-                    }
+                //                .toolbar{
+                //                    Button {
+                //
+                //                    } label: {
+                //                        Image(systemName: "plus")
+                //                    }
+                //                }
+                if let user = viewModel.user {
+                    profile(user: user)
                 }
-        }
+                else {
+                    Text("Loading")
+                }
+            }
+        }.navigationTitle("Profile")
+            .onAppear {
+                viewModel.fetchUser()
     }
-    
-}
+        
+        }
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width:125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name:")
+                Text(user.name)
+            }.padding()
+            HStack{
+                Text("Email:")
+                Text(user.email)
+            }.padding()
+            HStack{
+                Text("Member Since:")
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            
+        }.padding()
+      
+        Button("Logout"){
+            viewModel.logOut()
+        }
+        .tint( .red)
+        .padding()
+    }
+    }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
--- ToDoApp/ViewModel/ToDoListItemViewModel.swift
+++ ToDoApp/ViewModel/ToDoListItemViewModel.swift
@@ -5,9 +5,27 @@
 //
 //
 //ViewModel for single to do list item view (each now in items list)
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewModel: ObservableObject {
     init(){}
+    
+    func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+        
+    }
 }
 
